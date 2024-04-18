fc890052b70b750b276e183e533fa964a652d9e6
Josh McDannel <mcdannel@gmail.com>
2024-04-18 14:53:53 -0600

    Update MQTT broker and port in mqtt.mjs to include layoutId variable

--- src/broadcast.mjs
+++ src/broadcast.mjs
@@ -3,9 +3,11 @@ import server from './server.mjs'; // TODO: refactor to use event emitter
 import mqtt from './mqtt.mjs';
 import log from './utils/logger.mjs';
 
+const layoutId = process.env.LAYOUT_ID
+
 export const broadcast = async (data) => {
   try {
-    mqtt.send('@ttt/DCCEX.js', JSON.stringify(data));
+    mqtt.send(`@ttt/DCCEX.js/${layoutId}`, JSON.stringify(data));
     await server.send(data);
   } catch (err) {
     log.fatal('[BROADCAST] Error sending broadcast:', err);
--- src/mqtt.mjs
+++ src/mqtt.mjs
@@ -4,6 +4,7 @@ import dcc from './dcc.mjs';
 
 // const mqttBroker = 'mqtt://joshs-mac-mini.local'
 // const mqttPort = 5005;
+const layoutId = process.env.LAYOUT_ID
 const mqttBroker = process.env.VITE_MQTT_BROKER
 const mqttPort = 8081
 
@@ -23,10 +24,10 @@ const connect = () => {
   mqttClient.on('connect', () => {
     log.log('mqttClient connection successful')
     // mqttClient.publish('@ttt/DCCEX.js', '{ msg: "Hello mqtt" }')
-    mqttClient.publish('@ttt/DCCEX.js', JSON.stringify({ action: 'ack', payload: 'Hello mqtt' }))
+    mqttClient.publish(`@ttt/DCCEX.js/${layoutId}`, JSON.stringify({ action: 'ack', payload: 'Hello mqtt' }))
     // mqttClient.subscribe('ttt-dispatcher', handleSubscribeError)
     // mqttClient.subscribe('@ttt/dccex', handleSubscribeError)
-    mqttClient.subscribe('ttt-dcc', handleSubscribeError)
+    mqttClient.subscribe(`@ttt/dcc/${layoutId}`, handleSubscribeError)
 
   })
 
