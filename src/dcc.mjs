b81d2e654d386003cdbcebdd65bfe48c73175a9c
Josh McDannel <mcdannel@gmail.com>
2024-06-24 20:56:58 -0600

    chore: Refactor DCC module to improve code organization and remove unused code

--- src/dcc.mjs
+++ src/dcc.mjs
@@ -3,7 +3,6 @@ import serial from './serial.mjs'
 import { broadcast } from './broadcast.mjs'
 import log from './utils/logger.mjs'
 
-let port // serial port instance
 let isConnected = false // serial port connection status
 const baudRate = 115200
 
@@ -17,44 +16,44 @@ const getPorts = async () => {
 };
 
 const handleMessage = async (msg) => {
-  log.note('[DCC] handleMessage', msg)
+  log.note('handleMessage', msg)
   try {
     const { action, payload } = JSON.parse(msg)
     switch (action) {
       case 'connect':
-        connect(payload)
+        await connect(payload)
         break;
       case 'dcc':
-        send(payload)
+        await send(payload)
         break;
       case 'listPorts':
-        listPorts()
+        await listPorts()
         break;
       case 'power':
-        power(payload)
+        await power(payload)
         break;
       case 'throttle':
-        sendSpeed(payload)
+        await sendSpeed(payload)
         break;
       case 'turnout':
-        sendTurnout(payload)
+        await sendTurnout(payload)
         break;
       case 'output':
-        sendOutput(payload)
+        await sendOutput(payload)
         break;
       case 'function':
-        sendFunction(payload)
+        await sendFunction(payload)
         break;
       case 'getStatus':
       case 'status':
-        getStatus()
+        await getStatus()
         break;
       default:
         //noop
-        log.warn('[DCC] Unknown action in `handleMessage`', action, payload)
+        log.warn('Unknown action in `handleMessage`', action, payload)
     }
   } catch (err) {
-    log.fatal('[DCC] Error handling message:', err)
+    log.fatal('Error handling message:', err)
   }
 }
 
@@ -64,69 +63,69 @@ const handleConnectionMessage = async (payload) =>
 const send = async (data) => {
   try {
     const cmd = `<${data}>\n`
-    log.await('[DCC] Writing to port', data)
+    log.await('Writing to port', data)
     serial.send(cmd)
   } catch (err) {
-    log.fatal('[DCC] Error writting to port:', err)
+    log.fatal('Error writting to port:', err)
   }
 }
 
 const connect = async (payload) => {
   try {
-    log.star('[DCC] connect', payload)
+    log.star('connect', payload)
     const path = payload.serial
     if (isConnected && port) {
       await broadcast({ action: 'connected', payload: { path, baudRate } })
       return Promise.resolve(port)
     } else {
-      port = await serial.connect({ path, baudRate, handleConnectionMessage })
+      await serial.connect({ path, baudRate, handleMessage: handleConnectionMessage })
       await broadcast({ action: 'connected', payload: { path, baudRate } })
       isConnected = true
-      return Promise.resolve(port)
+      return Promise.resolve(isConnected)
     }
   } catch (err) {
-    log.fatal('[DCC] Error opening port: ', err)
+    log.fatal('Error opening port: ', err)
   }
 }
 
 const listPorts = async () => {
   const payload = await getPorts()
   await broadcast({ action: 'listPorts', payload })
-  log.star('[DCC] List ports', payload)
+  log.star('List ports', payload)
 }
 
 const getStatus = async () => {
   await broadcast({ action: 'status', payload: { isConnected } })
-  log.star('[DCC] getStatus', { action: 'status', payload: { isConnected } })
+  log.star('getStatus', { action: 'status', payload: { isConnected } })
 }
 
 const power = async (state) => {
   await send(state)
-  log.star('[DCC] Power', state)
+  log.star('Power', state)
 }
 
 const sendSpeed = async ({ address, speed }) => {
   const direction = speed > 0 ? 1 : 0
   const absSpeed = Math.abs(speed)
-  log.star('[DCC] Throttle', address, speed, direction)
+  log.star('Throttle', address, speed, direction)
   const cmd = `t ${address} ${absSpeed} ${direction}`
   await send(cmd)
 }
 
 const sendTurnout = async ({ turnoutId, state }) => {
-  log.star('[DCC] Turnout', turnoutId, state)
+  log.star('Turnout', turnoutId, state)
   const cmd = `T ${turnoutId} ${state ? 1 : 0}`
   await send(cmd)
 }
 
 const sendFunction = async ({ address, func, state }) => {
-  log.star('[DCC] Function', address, func)
+  log.star('Function', address, func)
   const cmd = `F ${address} ${func} ${state ? 1 : 0}`
   await send(cmd)
 }
 
 const sendOutput = async (payload) => {
-  log.star('[DCC] Output', payload)
+  log.star('Output', payload)
   const cmd = `Z ${payload.pin} ${payload.state ? 1 : 0}`
   await send(cmd)
 }
