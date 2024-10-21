40b2ee3b2ccd2812f5a20778351d8d4272a6851a
Josh McDannel <mcdannel@gmail.com>
2024-10-20 19:22:54 -0600

    feat: Add Login, LoginForm, LoginPage, and ProviderLogin components

---
+++ src/User/SignOut/SignOut.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { signOut } from 'firebase/auth'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+
+const auth = useFirebaseAuth()
+const user = useCurrentUser()
+
+function handleSignOut() {
+  signOut(auth).catch((reason) => {
+    console.error('Failed signOut', reason)
+    error.value = reason
+  })
+}
+</script>
+<template>
+  <button
+    class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+    @click="handleSignOut" 
+    :disabled="!user">Sign out
+  </button>
+</template>
