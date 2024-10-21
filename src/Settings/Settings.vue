fcdcdc36235665d4febd21783634700e12a0313a
Josh McDannel <mcdannel@gmail.com>
2024-10-20 19:22:36 -0600

    feat: Add new components for RoutesList, Settings, and SignalList

---
+++ src/Settings/Settings.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ModuleTitle from '@/Core/UI/ModuleTitle.vue'
+
+</script>
+<template>
+  <ModuleTitle menu="Settings" />  
+  <div
+    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
+  ></div>
+  <div class="grid grid-cols-2 gap-4">
+    <div
+      class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+    ></div>
+    <div
+      class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+    ></div>
+    <div
+      class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+    ></div>
+    <div
+      class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+    ></div>
+  </div>
+  
+</template>
