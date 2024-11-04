d2ebbed7c7475efd36300b9cc4c9388462756c0b
Josh McDannel <mcdannel@gmail.com>
2024-11-03 17:51:25 -0700

    Enhance Effect and Turnout components with loading states and improved UI; refactor layout for better responsiveness

--- src/effects/Effects.vue
+++ src/effects/Effects.vue
@@ -31,7 +31,18 @@ function filter(efxList) {
     <v-btn @click="showFilters = !showFilters" color="secondary" icon="mdi-filter"></v-btn>
   </header>
   <EffectFilters @devices="handleDeviceFilter" :class="showFilters ? 'visible' : 'hidden'"></EffectFilters>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 w-full">
+  <div class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 w-full">
+    <!-- <v-btn-toggle v-model="efxType" divided class="flex-wrap h-auto" size="x-large">
+      <v-btn v-for="efxOpt in efxTypes" :value="efxOpt.value" :key="efxOpt.value"
+        class="min-h-48 min-w-48 border"
+        color="purple">
+        <div class="flex flex-col">
+          <component :is="efxOpt.icon" :color="efxOpt.color" class="w-16 h-16 stroke-none"></component>
+          <div class="mt-4">{{ efxOpt.label }}</div>
+        </div>
+        
+      </v-btn>
+    </v-btn-toggle> -->
     <EffectItem v-for="item in filter(list)"
       :key="item.id" :efx="item" :efxId="item.id"></EffectItem>
   </div>
