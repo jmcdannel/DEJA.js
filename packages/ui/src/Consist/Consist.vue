551eaf1beb8b0f7632a68dfa032a89ef6e9d9fe9
Josh McDannel <mcdannel@gmail.com>
2025-10-17 09:55:42 -0600

    refactor(AddConsistLoco.vue, Consist.vue, ConsistLoco.vue): enhance UI components for improved usability and layout consistency

--- packages/ui/src/Consist/Consist.vue
+++ packages/ui/src/Consist/Consist.vue
@@ -16,19 +16,18 @@ const settingsDialogOpen = ref(false)
 <template>
   <v-sheet class="bg-gradient-to-r from-pink-500 to-indigo-800 flex flex-row gap-2 justify-between items-center p-2 rounded-2xl shadow-lg bg-gradient-border ">
     <slot name="title">
-      <h2 class="hidden md:block text-xl text-purple-400 opacity-70 font-bold">Consist</h2>
+      <h2 class="hidden md:block text-md text-purple-400 opacity-70 font-bold">Consist</h2>
     </slot>
-    <ol v-if="loco?.consist?.length" class="flex flex-row items-center justify-between overflow-x-auto p-1">
-      <li>
+    <div v-if="loco?.consist?.length" class="flex flex-row items-center justify-between overflow-x-auto p-1">
         <v-chip 
-          class="ma-1 hidden md:block" 
+          class="ma-1 hidden md:flex" 
           variant="elevated" 
           prepend-icon="mdi-arrow-left-circle"
           :color="'primary'"
+          size="small"
         >
           {{ loco?.address }}
         </v-chip>
-      </li>
       <template v-for="cloco in loco?.consist" :key="cloco">
         <v-chip 
           class="ma-1" 
@@ -36,15 +35,13 @@ const settingsDialogOpen = ref(false)
           :append-icon="!cloco.direction ? 'mdi-arrow-right-circle' : undefined"
           :prepend-icon="cloco.direction ? 'mdi-arrow-left-circle' : undefined"
           :color="'secondary'"
-          size="x-small"
+          size="small"
         >
           {{ cloco?.address }}
         </v-chip>
       </template>
-      <li>
-        <v-btn @click="settingsDialogOpen = true" color="purple" icon="mdi-cog" size="24" variant="tonal" />
-      </li>
-    </ol>
+      <v-btn @click="settingsDialogOpen = true" color="purple" icon="mdi-cog" size="24" variant="tonal" />
+    </div>
     <template v-else>
       <v-btn color="purple" @click="settingsDialogOpen = true" icon="mdi-cogs" size="24" variant="tonal" />
     </template>
@@ -53,7 +50,8 @@ const settingsDialogOpen = ref(false)
   <v-dialog 
     v-model="settingsDialogOpen" 
     transition="dialog-bottom-transition"
-    min-width="400"
+    width="auto"
+    min-width="420"
     max-width="80vw">
     <template v-slot:default>
       <EditConsist :loco="loco" color="purple" @close="settingsDialogOpen = false" />
