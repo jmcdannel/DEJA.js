ee885402387242a430ee578d7548b5726c07bd70
Josh McDannel <mcdannel@gmail.com>
2026-03-13 20:20:12 -0600

    fix(www): downgrade sanity to v3 for React 18 compatibility and fix Studio route

--- apps/dejajs-www/app/studio/[[...tool]]/page.tsx
+++ apps/dejajs-www/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,7 @@
+'use client'
+
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
-
-export const dynamic = 'force-static'
-export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
