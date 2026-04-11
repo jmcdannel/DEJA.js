3b4319d70036327e3eb4e832f361efe4dadb014e
Josh McDannel <mcdannel@gmail.com>
2026-04-10 18:34:21 -0600

    feat(command-palette): 🔍 add header trigger button

---
+++ apps/throttle/src/command-palette/CommandPaletteTrigger.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useCommandPalette } from './useCommandPalette'
+
+const { open } = useCommandPalette()
+
+function handleClick() {
+  open()
+}
+</script>
+
+<template>
+  <v-btn
+    size="small"
+    variant="text"
+    class="ma-1"
+    aria-label="Command palette (Cmd+K)"
+    @click="handleClick"
+  >
+    <v-icon>mdi-magnify</v-icon>
+    <v-tooltip activator="parent" location="bottom">
+      Command palette
+      <span class="text-caption opacity-60 ml-1">⌘K</span>
+    </v-tooltip>
+  </v-btn>
+</template>
